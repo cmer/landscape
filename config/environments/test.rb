4126d2c9b4d7c89582e3edc3f1f9dd11eff94a6a
Carl Mercier <carl@carlmercier.com>
2009-07-10 14:52:28 -0400

    rescue exception when cant load ruby-debug

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,4 +27,6 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-require 'ruby-debug'
+begin
+  require 'ruby-debug'
+rescue; end
